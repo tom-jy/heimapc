2c565fd3e7dfd5bc6604fcd8742b6aecb54b2ca1
xjy <xjy.com>
2020-05-08 20:25:13 +0800

    配置axios-全局注册使用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import ElementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/style/index.less'
+import axios from 'axios'// 引入axios
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+Vue.prototype.$axios = axios // 将axios 给到VUE的原型对象
 Vue.config.productionTip = false
 Vue.use(ElementUi)
 new Vue({
