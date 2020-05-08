a2e6a76582cddacf63c182d4430c22906247f167
xjy <xjy.com>
2020-05-09 03:02:11 +0800

    主页布局容器完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
-      <h1>我是主页</h1>
-      </div>
+  <!-- 外部容器 -->
+  <el-container>
+    <!-- 侧边栏容器 -->
+    <el-aside>
+      <h1>侧边栏</h1>
+    </el-aside>
+    <!-- 右侧区域 -->
+    <el-container>
+      <!-- 标题栏 -->
+      <el-header>
+        <h1>标题栏</h1>
+      </el-header>
+      <!-- 主要内容区域 -->
+      <el-main>
+        <h1>主要内容区域</h1>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
