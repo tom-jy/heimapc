6614c7ac2896bad45394b99efb514186c4bec401
xjy <xjy.com>
2020-05-11 06:37:40 +0800

    获取用户信息

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -6,9 +6,9 @@
         </el-col>
         <el-col class="right" :span="12">
             <el-row type="flex" justify="end" align="middle">
-                <img src="http://img0.imgtn.bdimg.com/it/u=3256100974,305075936&fm=26&gp=0.jpg" alt="">
+                <img :src="userInfo.photo" alt="">
                 <el-dropdown>
-                    <span class="el-dropdown-link">下拉菜单
+                    <span class="el-dropdown-link">{{userInfo.name}}
                          <i class="el-icon-arrow-down el-icon--right"></i>
                     </span>
                     <el-dropdown-menu>
@@ -25,7 +25,27 @@
 
 <script>
 export default {
-
+  // 现在data中定义一个接收信息的对象
+  data () {
+    return {
+      userInfo: {}
+    }
+  },
+  //   用钩子函数created获取用户信息
+  created () {
+    //   拿到钥匙，
+    const token = localStorage.getItem('use-token')
+    this.$axios({
+      url: 'user/profile',
+      headers: {
+        // 把token放进headers中，格式要求前边要跟Bearer空格
+        Authorization: `Bearer ${token}`
+      }
+    }).then(result => {
+      // 成功拿到钥匙之后，赋值给接收对象userInfo
+      this.userInfo = result.data.data // 最后去v-bind给页面
+    })
+  }
 }
 </script>
 
