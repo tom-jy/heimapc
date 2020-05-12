1c0c356fa88c9f47baa1fe4c5067cd2fe5b41c36
xjy <xjy.com>
2020-05-12 10:34:14 +0800

    请求拦截器

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import ElementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/style/index.less'
-import axios from '@/utils/request.js'// 引入axios
-
+import Components from './components'
+import axios from './utils/request.js'// 引入axios
+// axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.prototype.$axios = axios // 将axios 给到VUE的原型对象
 Vue.config.productionTip = false
+Vue.use(Components)
 Vue.use(ElementUi)
 new Vue({
   router,
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,21 +1,16 @@
-// 封装- 引入axios
-import axios from 'axios'
-// 引入请求头地址
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+import axios from 'axios' // 引入
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'// 引入baseURL请求地址
 
-// 注册请求拦截器 参数为两个回调函数
+// 注册请求拦截器
 axios.interceptors.request.use(function (config) {
-// 成功之后执行，config参数表示所有的axios请求信息
+// 成功执行-回调函数，config是一个对象，代表所有请求信息
 // 获取token
   const token = localStorage.getItem('use-token')
-  // 在返回之前，  统一注入token
+  // 在返回之前统一注入token
   config.headers.Authorization = `Bearer ${token}`
   return config
 }, function (error) {
-// 失败之后执行，可赢promise。
-// 失败之后会有一个错误，直接reject错误即可  reject会直接进入到axios的catch中
+// 失败时会有一个错误，直接reject错误即可，reject会进去axios的catch中
   return Promise.reject(error)
 })
-
-// 导出
-export default axios
+export default axios // 导出
