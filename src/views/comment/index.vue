4334fd390c7d2bdc14602b35f2ff9ba953755cb6
xjy <xjy.com>
2020-05-12 13:27:51 +0800

    评论列表-table

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,10 +1,33 @@
 <template>
-  <div>我是谁 我是评论</div>
+<!-- 用el卡片 具名slot放在header-->
+  <el-card>
+      <!-- 引用面包屑 -->
+      <breadcrub slot="header">
+           <template slot="tittle">
+            评论管理
+           </template>
+      </breadcrub>
+<!-- 评论列表 -->
+      <el-table :data="list">
+          <!-- lable表头 width宽 prop显示字段-->
+          <el-table-column width="600px" label="标题"></el-table-column>
+          <el-table-column label="评论状态"></el-table-column>
+          <el-table-column label="总评论数"></el-table-column>
+          <el-table-column label="粉丝评论数"></el-table-column>
+          <el-table-column label="操作"></el-table-column>
+      </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      list: [
 
+      ]
+    }
+  }
 }
 </script>
 
