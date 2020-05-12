b669b9e77e6f7d11ea8459721bb2989be8ad4c43
xjy <xjy.com>
2020-05-12 12:38:24 +0800

    面包屑

---
+++ src/components/common/breadcrub.vue
@@ -0,0 +1,20 @@
+<template>
+<!-- separator 分隔符 -->
+   <el-breadcrumb separator=">">
+       <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+       <!-- 第二个内容是动态的 需要用props或插槽slot方式传进来 -->
+       <el-breadcrumb-item>
+           <slot name="tittle"></slot>
+       </el-breadcrumb-item>
+   </el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,10 @@
 import layoutAsaid from './home/layout-said'
 import layoutHeader from './home/layout-header'
-
+import Breadcrub from './common/breadcrub.vue'
 export default {
   install: function (Vue) {
     Vue.component('layout-said', layoutAsaid)
     Vue.component('layout-header', layoutHeader)
+    Vue.component('breadcrub', Breadcrub) // 全局注册面包屑组件
   }
 }
