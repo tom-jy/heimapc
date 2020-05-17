d81763380757fd71d3c17577c0227f5ec7cc8225
xjy <xjy.com>
2020-05-17 10:52:05 +0800

    素材管理-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -7,11 +7,23 @@
           素材管理
       </template>
     </breadcrub>
+
+    <el-tabs v-model="activeName">
+        <!-- label表示显示标签的名称，name表示选中的值 -->
+         <el-tab-pane label="全部" name="all">全部</el-tab-pane>
+         <el-tab-pane label="收藏" name="collect">收藏</el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      activeName: 'all' // 表示默认选中激活的值
+    }
+  }
+}
 </script>
 
 <style>
